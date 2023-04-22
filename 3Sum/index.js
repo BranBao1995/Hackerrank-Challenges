a540e3a435f3a425aa0d17037b91ee183f1ef602
BranBao1995 <brandon.0702@hotmail.com>
2023-04-22 14:59:54 -0400

    added a new challenge

--- 3Sum/index.js
+++ 3Sum/index.js
@@ -1,65 +1,34 @@
 // two-pointer approach
 
-// const nums = [-1, 0, 1, 2, -1, -4];
-
-// var threeSum = function (nums) {
-//   nums.sort((a, b) => a - b);
-
-//   const triplets = [];
-
-//   for (let index = 0; index < nums.length - 1; index++) {
-//     if (nums[index] != nums[index - 1]) {
-//       let left = index + 1;
-//       let right = nums.length - 1;
-
-//       while (left < right) {
-//         const sum = nums[index] + nums[left] + nums[right];
-//         if (sum === 0) {
-//           triplets.push([nums[index], nums[left], nums[right]]);
-//           while (nums[left] === nums[left + 1]) {
-//             left++;
-//           }
-//           while (nums[right] === nums[right - 1]) {
-//             right--;
-//           }
-//           left++;
-//           right--;
-//         } else if (sum < 0) {
-//           left++;
-//         } else if (sum > 0) {
-//           right--;
-//         }
-//       }
-//     }
-//   }
-
-//   console.log(triplets);
-//   return triplets;
-// };
-
-// threeSum(nums);
-
-// no-sort hashset approach
-
 const nums = [-1, 0, 1, 2, -1, -4];
 
 var threeSum = function (nums) {
-  const map = new Map();
+  nums.sort((a, b) => a - b);
 
-  const triplets = new Set();
+  const triplets = [];
 
-  const duplicates = new Set();
+  for (let index = 0; index < nums.length - 1; index++) {
+    if (nums[index] != nums[index - 1]) {
+      let left = index + 1;
+      let right = nums.length - 1;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (duplicates.has(nums[i]) === false) {
-      duplicates.add(nums[i]);
-
-      for (let j = i + 1; j < nums.length; j++) {
-        const complement = -nums[i] - nums[j];
-        if (map.has(complement) && map.get(complement) === i) {
-          triplets.add([nums[i], nums[j], complement].sort((a, b) => a - b));
+      while (left < right) {
+        const sum = nums[index] + nums[left] + nums[right];
+        if (sum === 0) {
+          triplets.push([nums[index], nums[left], nums[right]]);
+          while (nums[left] === nums[left + 1]) {
+            left++;
+          }
+          while (nums[right] === nums[right - 1]) {
+            right--;
+          }
+          left++;
+          right--;
+        } else if (sum < 0) {
+          left++;
+        } else if (sum > 0) {
+          right--;
         }
-        map.set(nums[j], i);
       }
     }
   }
@@ -69,3 +38,34 @@ var threeSum = function (nums) {
 };
 
 threeSum(nums);
+
+// no-sort hashset approach
+
+// const nums = [-1, 0, 1, 2, -1, -4];
+
+// var threeSum = function (nums) {
+//   const map = new Map();
+
+//   const triplets = new Set();
+
+//   const duplicates = new Set();
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (duplicates.has(nums[i]) === false) {
+//       duplicates.add(nums[i]);
+
+//       for (let j = i + 1; j < nums.length; j++) {
+//         const complement = -nums[i] - nums[j];
+//         if (map.has(complement) && map.get(complement) === i) {
+//           triplets.add([nums[i], nums[j], complement].sort((a, b) => a - b));
+//         }
+//         map.set(nums[j], i);
+//       }
+//     }
+//   }
+
+//   console.log(triplets);
+//   return triplets;
+// };
+
+// threeSum(nums);
