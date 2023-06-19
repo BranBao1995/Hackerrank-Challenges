9c69235bc992ddb8be31b3906723e65b854a05a4
BranBao1995 <brandon.0702@hotmail.com>
2023-06-19 18:08:30 -0400

    added a new challenege

--- TraverseChessBoard/index.js
+++ TraverseChessBoard/index.js
@@ -23,7 +23,29 @@ const traverse = function (m, n, memo = {}) {
   return memo[key];
 };
 
-console.log(traverse(18, 18));
+// console.log(traverse(18, 18));
+
+// Tabulation (bottom-up) approach
+
+const traverse_tab = function (m, n) {
+  const table = Array(m + 1)
+    .fill()
+    .map(() => Array(n + 1).fill(0));
+
+  table[1][1] = 1;
+
+  for (let i = 0; i <= m; i++) {
+    for (let j = 0; j <= n; j++) {
+      const current = table[i][j];
+      if (j + 1 <= n) table[i][j + 1] += current;
+      if (i + 1 <= m) table[i + 1][j] += current;
+    }
+  }
+
+  return table[m][n];
+};
+
+console.log(traverse_tab(18, 18));
 
 // Brute force approach
 
---
+++ canSum/index.js
@@ -0,0 +1,54 @@
+// Using tabulation (bottom-up) approach
+
+const target = 300;
+const nums = [7, 14];
+
+const canSum_tab = function (targetSum, nums) {
+  const results = Array(targetSum + 1).fill(false);
+
+  results[0] = true;
+
+  for (let i = 0; i <= targetSum; i++) {
+    if (results[i] === true) {
+      for (let j = 0; j < nums.length; j++) {
+        if (i + j <= targetSum) {
+          results[i + nums[j]] = true;
+        }
+      }
+    }
+  }
+
+  return results[targetSum];
+};
+
+// console.log(canSum_tab(target, nums));
+
+// Using recursion with memoization (top-down) approach
+
+const canSum_memo = function (targetSum, nums, memo = {}) {
+  if (targetSum in memo) {
+    return memo[targetSum];
+  }
+
+  if (targetSum === 0) {
+    return true;
+  }
+
+  if (targetSum < 0) {
+    return false;
+  }
+
+  for (let num of nums) {
+    const remainder = targetSum - num;
+    if (canSum_memo(remainder, nums, memo) === true) {
+      memo[targetSum] = true;
+      return memo[targetSum];
+    }
+  }
+
+  memo[targetSum] = false;
+
+  return memo[targetSum];
+};
+
+console.log(canSum_memo(target, nums));
