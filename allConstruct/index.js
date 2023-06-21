691aac87dba8a3bb87f29b5fba2e6ada7e7a261f
BranBao1995 <brandon.0702@hotmail.com>
2023-06-21 16:58:35 -0400

    added a new problem

--- allConstruct/index.js
+++ allConstruct/index.js
@@ -1,6 +1,8 @@
 const target = "purple";
 const wordBank = ["purp", "p", "ur", "le", "purpl"];
 
+// Using recursion with memoization (top-down) approach
+
 const allConstruct = function (target, wordBank, memo = {}) {
   if (target in memo) return memo[target];
 
@@ -23,4 +25,27 @@ const allConstruct = function (target, wordBank, memo = {}) {
   return memo[target];
 };
 
-console.log(allConstruct(target, wordBank));
+// console.log(allConstruct(target, wordBank));
+
+// Using tabulation (bottom-up) approach
+
+const allConstruct_tab = function (target, wordBank) {
+  const table = Array(target.length + 1)
+    .fill()
+    .map(() => []);
+
+  table[0] = [[]];
+
+  for (let i = 0; i <= target.length; i++) {
+    for (let word of wordBank) {
+      if (target.slice(i, i + word.length) === word) {
+        const combinations = table[i].map((el) => [...el, word]);
+        table[i + word.length] = [...table[i + word.length], ...combinations];
+      }
+    }
+  }
+
+  return table[target.length];
+};
+
+console.log(allConstruct_tab(target, wordBank));
